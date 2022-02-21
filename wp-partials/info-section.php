41946a91ae02817513cb6ccac62cb5c6ee8f861b
Dragana Djukic <dragana.djukic95@hotmail.com>
2022-02-21 13:49:39 +0100

    git commit -m 'new-changes'

---
+++ wp-partials/info-section.php
@@ -0,0 +1,20 @@
+<section class="contact-section">
+            <div class="container text-center">
+                <h2>Lawyer - Attorneys at Law</h2>
+                <p class='animation slide-top' data-animation="slide-top" data-delay='0s'>
+                    <span class="fas fa-map-marker-alt"></span>
+                    146 Douglas Dale Suite 343
+                </p>
+
+                <p class='animation slide-top' data-animation="slide-top" data-delay='0.2s'>
+                    <span class="fas fa-phone-alt"></span>
+                    <a href="#" class="tel:560-748-5959">560-748-5959</a>
+                </p>
+
+                <p class='animation slide-top' data-animation="slide-top" data-delay='0.4s'>
+                    <span class="far fa-envelope"></span>
+                    <a href="#" class="mailto:cullen_mueller@yahoo.com">cullen_mueller@yahoo.com</a>
+                </p>
+
+            </div>
+        </section>
